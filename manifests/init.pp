5b90c4ef72be91b64aebeffd8e57c658bd5a7ada
Arnaud Gomes <Arnaud.Gomes@ircam.fr>
2013-04-08 15:07:45 +0200

    initial import

---
+++ manifests/init.pp
@@ -0,0 +1,29 @@
+# == Class: zfs
+#
+# This module installs ZFS from http://zfsonlinux.org/ on Linux hosts. It
+# does not manage pools, just the kernel-level support.
+#
+# === Parameters
+#
+# None.
+#
+# === Variables
+#
+# None.
+#
+# === Examples
+#
+#  class { zfs: }
+#
+# === Authors
+#
+# Arnaud Gomes-do-Vale <Arnaud.Gomes@ircam.fr>
+#
+# === Copyright
+#
+# Copyright 2013 Arnaud Gomes-do-Vale
+#
+class zfs {
+
+
+}
